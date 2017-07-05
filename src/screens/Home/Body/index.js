483862b7d87de46a608d482d5ce4b391a31c2478
gbenga504 <daveanifowoshe@ymail.com>
2017-07-05 22:28:19 +0100

    created a button for the home segment

---
+++ src/screens/Home/Body/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./index.css";
+
+export const Body = () => {
+  return (
+    <div className="row body">
+      <div className="col-xs-12 body_container">
+        <div className="row">
+          <ImageRenderer />
+          <TextRenderer />
+          <ButtonRenderer>
+            <Button /> <Button style={{marginLeft:"300px"}}/>
+          </ButtonRenderer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ImageRenderer = () => {
+  return (
+    <div className="col-xs-1">
+      <div className="imageRenderer">
+        <img src={""} alt="" />
+      </div>
+    </div>
+  );
+};
+
+const TextRenderer = () => {
+  return (
+    <div className="col-xs-11 textRenderer">
+      Contactnaire is a game originally designed to act as a clone for the popular game "WHO WANTS TO BE A MILLIONAIRE". This game offers more real life scenarios to make the game more communicative and engaging. 
+      To start, continue with your facebook or twitter account and have fun. No worries, the game and site has been heavily minimized while providing stable functionalities. <br /><br />
+      <center><b><h4>SO WE HAVE GOT YOU COVERED!!!</h4></b></center>
+    </div>
+  );
+};
+
+const ButtonRenderer = (props) => {
+  return (
+    <div className="row buttonRenderer">
+      <div className="col-xs-offset-3 col-xs-6">
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div className="social_button" style={{...props.style}}>
+
+    </div>
+  )
+}
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,14 @@
 import React, { PureComponent } from "react";
 import { Layout } from "../../containers/Layout";
 import {Header} from "./Header";
+import {Body} from './Body';
 
 export default class Home extends PureComponent {
   render() {
     return (
         <Layout>
             <Header />
+            <Body />
         </Layout>
     )
   }
