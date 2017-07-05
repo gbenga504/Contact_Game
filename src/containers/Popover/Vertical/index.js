01f388b7e94dcd947c298c6b96005654e5fbb124
gbenga504 <daveanifowoshe@ymail.com>
2017-07-05 20:27:50 +0100

    added some contents to the popover box

--- src/containers/Popover/Vertical/index.js
+++ src/containers/Popover/Vertical/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 export const Vertical = () => {
-    return (
-        <div className="vertical_options_container">
-
-        </div>
-    )
-} 
+  return (
+    <div className="vertical_options_holder">
+      <div className="vertical_options_container">
+        <div className="vetical_options">Logout</div>
+        <div className="vetical_options">Dashboard</div>
+        <div className="vetical_options">share</div>
+      </div>
+    </div>
+  );
+};
