a07b73a5ffac204581f292092057c05e1c5fbc25
gbenga504 <daveanifowoshe@ymail.com>
2017-07-09 12:52:59 +0100

    added flow to the app static typing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
+import Game from "./screens/Game";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact component={Home} path="/" />
+        <Route component={Game} path="/game" />        
       </Switch>
     </Router>
   );
---
+++ src/screens/Game/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+export default class Game extends PureComponent {
+  render() {
+    return (
+        <div className="row">
+            <div className="col-xs-12">
+
+            </div>
+        </div>
+    )
+  }
+}
