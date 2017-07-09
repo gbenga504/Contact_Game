028ab5da796f2d223fa2acdae975b26464aba8de
gbenga504 <daveanifowoshe@ymail.com>
2017-07-09 17:27:19 +0100

    done with creating the normal changes for the NickName segment

--- src/screens/Game/NickName/index.js
+++ src/screens/Game/NickName/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./index.css";
 
-export const NickName = () => {
+export const NickName = props => {
   return (
-    <div className="row">
-      <div className="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-4 col-md-4 nickName">
+    <div className="row nickName">
+      <div className="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-4 col-md-4">
         <div className="row">
-            <input className="col-xs-12" type="text"/>
+          <input
+            className="col-xs-12"
+            type="text"
+            placeholder="Enter your game nick name"
+          />
         </div>
         <div className="row nickName_continue_btn_holder">
-            <div className="col-xs-12 col-sm-offset-4 col-sm-4">
-                <input type="button" value="continue"/>
-            </div>
+          <div className="col-xs-12 col-sm-offset-4 col-sm-4">
+            <input type="button" value="continue" />
+          </div>
         </div>
       </div>
     </div>
--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -1,14 +1,14 @@
 import React, { PureComponent } from "react";
-import {NickName} from "./NickName";
+import { NickName } from "./NickName";
 
 export default class Game extends PureComponent {
   render() {
     return (
-        <div className="row">
-            <div className="col-xs-12">
-              <NickName />
-            </div>
+      <div className="row" style={{ height: "100%" }}>
+        <div className="col-xs-12" style={{ height: "100%" }}>
+          <NickName />
         </div>
-    )
+      </div>
+    );
   }
 }
